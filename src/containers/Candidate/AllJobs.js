77acac159913b4f40c3dc266f76aa1a01efb6ea6
KishanSinghChauhan <kishancoer7071@gmail.com>
2021-01-07 05:09:14 +0530

    style candidate all job page | [Kishan]

--- src/containers/Candidate/AllJobs.js
+++ src/containers/Candidate/AllJobs.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Pagination from "@material-ui/lab/Pagination";
+import "./candidate_scss/AllJobs.scss";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 
 const AllJobs = () => {
   const [data, setData] = useState([]);
   const [id, setId] = useState("");
-  const [page,setPage] = useState(1);
+  const [page, setPage] = useState(1);
   const availableJobs = (page) => {
-    fetch(`https://jobs-api.squareboat.info/api/v1/candidates/jobs?page=${page}`, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-        Authorization: localStorage.getItem("jwt"),
-      },
-    })
+    fetch(
+      `https://jobs-api.squareboat.info/api/v1/candidates/jobs?page=${page}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: localStorage.getItem("jwt"),
+        },
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data.data);
@@ -49,17 +54,15 @@ const AllJobs = () => {
   };
   const jobDetail = data.map((d) => {
     return (
-      <div
-        key={d.id}
-        className="card col-md-3 col-6"
-        style={{ minHeight: "200px" }}
-      >
-        <div>
-          <h6>{d.title}</h6>
-          <p>{d.description}</p>
-          <span>{d.location}</span>
-        </div>
+      <div key={d.id} className="col-md-3 col-6 job-det">
+        <h6 className="job-title">{d.title}</h6>
+        <p className="job-para">{d.description}</p>
+        <p className="location">
+          <LocationOnOutlinedIcon style={{ color: "#43AFFF" }} />
+          {d.location}
+        </p>
         <button
+          className="apply-job"
           onClick={() => {
             handleApply(d);
           }}
@@ -76,10 +79,14 @@ const AllJobs = () => {
   };
 
   return (
-    <div>
-      <h4>Jobs for you</h4>
-      <div className="row">{jobDetail}</div>
-      <Pagination count={4} shape="rounded" onChange={handleChange}/>
+    <div className="all-job-main">
+      <div className="all-job">
+        <h6 className="all-job-head">Jobs for you</h6>
+        <div className="row">{jobDetail}</div>
+        <div className="job-pagination">
+          <Pagination count={4} shape="rounded" onChange={handleChange} />
+        </div>
+      </div>
     </div>
   );
 };
